24acd83fb3a18fef8cd6e1dc3fa6af81524e13e9
celinaluetticke-dev <celina.luetticke@gmail.com>
2025-12-04 21:46:37 +0100

    Hausaufgabe 3

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,5 +1,16 @@
 package h3;
 
 public class H3_main {
+	public static void main(String[] args) {
 
+		Mensch m = new Mensch();
+
+		m.setName("Elsa");
+		m.setGebJahr(1965);
+		m.setAlter();
+
+		System.out.println("Name: " + m.getName());
+		System.out.println("Geburtsjahr: " + m.getGebJahr());
+		System.out.println("Alter: " + m.getAlter());
+	}
 }
